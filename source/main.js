fea394bbfc4d24126a68ebc07e643d4264e03512
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-03-13 16:24:47 -0300

    adionando btn

--- source/main.js
+++ source/main.js
@@ -1,6 +1,14 @@
 const btnDrawer =  document.getElementById("btnDrawer")
 const menu =  document.getElementById("menu")
+const btnToggled =  document.getElementById("toggled")
 
 btnDrawer.addEventListener("click", ()=>{
     menu.style.display == "none" || menu.style.display == ""? menu.style.display = "flex" : menu.style.display = "none";
+ })
+
+ btnToggled.addEventListener("click", ()=>{
+    if(btnToggled.value == "<i class='bx bx-toggle-left'></i>"){
+        btnToggled.innerHTML = "<i class='bx bx-toggle-right'></i>"
+    }else{btnToggled.innerHTML = "<i class='bx bx-toggle-left'></i>"
+    }
  })
